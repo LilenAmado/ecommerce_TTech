09860be65565aa1481ff8d47f997bedf927111e8
Lilén Amado <lilenanahiamado@gmail.com>
2025-10-16 19:43:50 -0300

    Modificaciones para Netlify

--- src/containers/ShoppingCart.jsx
+++ src/containers/ShoppingCart.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { ProyectContext } from '../context/proyectContext';
+import { ProyectContext } from '../context/ProyectContext.jsx';
 import { Link } from 'react-router-dom';
 import Button from '../components/General/Button/Button';
 import Text from '../components/General/Text/Text';
